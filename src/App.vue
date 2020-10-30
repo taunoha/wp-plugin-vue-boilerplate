4b1c63bb62e7b863099758dd66c4a73114af458b
Tauno Hanni <tauno@loomdigital.ee>
2020-10-30 22:16:13 +0200

    Added the Components folder

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
     <article>
         <h1>Hello World!</h1>
+        <HelloWorld></HelloWorld>
     </article>
 </template>
 
 <script>
+
+import HelloWorld from '@/components/HelloWorld';
+
 export default {
-    name : 'App'
+    name : 'App',
+    components: {
+        HelloWorld
+    }
 }
 </script>
 
