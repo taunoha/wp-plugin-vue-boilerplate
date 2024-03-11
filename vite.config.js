c97ea494946d3a0b2b5992a1a27451a68778c6c0
Tauno Hanni <taunoh@gmail.com>
2024-03-11 22:56:20 +0200

    feat: Vue3 and Vite 5 support.

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ const filename = '{plugin-shortcode}';
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
-    'process.env': process.env
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
   },
   plugins: [
     vue(), 
