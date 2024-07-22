79e872de7d99e616130827ba8b12e5e88f28ffd0
Tauno Hanni <taunoh@gmail.com>
2024-07-22 18:13:18 +0300

    chore: Implement @wordpress/i18n and @wordpress/interactivity utilities

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -2,9 +2,11 @@ import { createI18n, sprintf as wpSprintf } from "@wordpress/i18n";
 
 const i18n = createI18n();
 
-export const __ = i18n.__;
-export const _x = i18n._x;
-export const _n = i18n._n;
-export const _nx = i18n._nx;
+export const __ = (text = "") => i18n.__(text);
+export const _x = (text = "", context = "") => i18n._x(text, context);
+export const _n = (text = "", textPlural = "", number = 0) =>
+  wpSprintf(i18n._n(text, textPlural, number), number);
+export const _nx = (text = "", textPlural = "", number = 0, context = "") =>
+  wpSprintf(i18n._nx(text, textPlural, number, context), number);
 export const sprintf = wpSprintf;
 export const setLocaleData = i18n.setLocaleData;
--- vite-plugins/gettext-extractor-for-wordpress.js
+++ vite-plugins/gettext-extractor-for-wordpress.js
@@ -3,21 +3,21 @@ import fs from 'fs';
 import { GettextExtractor, JsExtractors } from 'gettext-extractor';
 
 const sanitizeText = (text) => text.replace(/\\/g, '\\\\')
-.replace(/\u0008/g, '\\b')
-.replace(/\t/g, '\\t')
-.replace(/\n/g, '\\n')
-.replace(/\f/g, '\\f')
-.replace(/\r/g, '\\r')
-.replace(/'/g, '\\\'')
-.replace(/"/g, '\\"');
+  .replace(/\u0008/g, '\\b')
+  .replace(/\t/g, '\\t')
+  .replace(/\n/g, '\\n')
+  .replace(/\f/g, '\\f')
+  .replace(/\r/g, '\\r')
+  .replace(/'/g, '\\\'')
+  .replace(/"/g, '\\"');
 
 export default function gettextExtractorForWordpress(options) {
 
   const opts = {
-      path: '',
-      context: '',
-      domain: '',
-      ...options
+    path: '',
+    context: '',
+    domain: '',
+    ...options
   };
 
   return {
@@ -27,98 +27,98 @@ export default function gettextExtractorForWordpress(options) {
       var php = ['<?php', '', "defined('ABSPATH') or die();", '', 'return array('];
       var messages = [];
 
-      if( this.meta.watchMode ) {
-          return;
+      if (this.meta.watchMode) {
+        return;
       }
 
       const extractor = new GettextExtractor();
 
       extractor
-          .createJsParser([
-              JsExtractors.callExpression(['__'], {
-                  arguments: {
-                      text: 0,
-                  }
-              }),
-          ])
-          .parseFilesGlob('./src/**/*.vue')
-          .parseFilesGlob('./src/**/*.js');
-
-    extractor
-          .createJsParser([
-              JsExtractors.callExpression(['_n'], {
-                  arguments: {
-                      text: 0,
-                      textPlural: 1,
-                      context: 2
-                  }
-              }),
-          ])
-          .parseFilesGlob('./src/**/*.vue')
-          .parseFilesGlob('./src/**/*.js');
-
-    extractor
         .createJsParser([
-            JsExtractors.callExpression(['_x'], {
-                arguments: {
-                    text: 0,
-                    context: 1
-                }
-            }),
+          JsExtractors.callExpression(['__'], {
+            arguments: {
+              text: 0,
+            }
+          }),
         ])
         .parseFilesGlob('./src/**/*.vue')
         .parseFilesGlob('./src/**/*.js');
 
-    extractor
+      extractor
         .createJsParser([
-            JsExtractors.callExpression(['_nx'], {
-                arguments: {
-                    text: 0,
-                    textPlural: 1,
-                    count: 2,
-                    context: 3
-                }
-            }),
+          JsExtractors.callExpression(['_n'], {
+            arguments: {
+              text: 0,
+              textPlural: 1,
+              context: 2
+            }
+          }),
+        ])
+        .parseFilesGlob('./src/**/*.vue')
+        .parseFilesGlob('./src/**/*.js');
+
+      extractor
+        .createJsParser([
+          JsExtractors.callExpression(['_x'], {
+            arguments: {
+              text: 0,
+              context: 1
+            }
+          }),
+        ])
+        .parseFilesGlob('./src/**/*.vue')
+        .parseFilesGlob('./src/**/*.js');
+
+      extractor
+        .createJsParser([
+          JsExtractors.callExpression(['_nx'], {
+            arguments: {
+              text: 0,
+              textPlural: 1,
+              count: 2,
+              context: 3
+            }
+          }),
         ])
         .parseFilesGlob('./src/**/*.vue')
         .parseFilesGlob('./src/**/*.js');
 
       messages = extractor.getMessages();
 
-      messages.forEach( (value) => {
+      messages.forEach((value) => {
         const { text, textPlural, context } = value;
         const _text = sanitizeText(text);
         const _textPlural = sanitizeText(textPlural || '');
 
         php.push(`  // ${value.references.join(', ')}`);
 
-        if( context ) {
-            if (textPlural) {
-                php.push(`  "${context}\u0004${_text}" => array(_x("${_text}", '${context}', '${opts.domain}'), _x("${_textPlural}", '${context}', '${opts.domain}')),`);
-            } else {
-                php.push(`  "${context}\u0004${_text}" => array(_x("${_text}", '${context}', '${opts.domain}')),`);
-            }
+        if (context) {
+          if (textPlural) {
+            php.push(`  "${context}\u0004${_text}" => array(_x("${_text}", '${context}', '${opts.domain}'), _x("${_textPlural}", '${context}', '${opts.domain}')),`);
+          } else {
+            php.push(`  "${context}\u0004${_text}" => array(_x("${_text}", '${context}', '${opts.domain}')),`);
+          }
         } else {
-            if (textPlural) {
-                php.push(`  "${_text}" => array(__("${_text}", '${opts.domain}'), __("${_textPlural}", '${opts.domain}')),`);
-            } else {
-                php.push(`  "${_text}" => array(__("${_text}", '${opts.domain}')),`);
-            }
+          if (textPlural) {
+            php.push(`  "${_text}" => array(__("${_text}", '${opts.domain}'), __("${_textPlural}", '${opts.domain}')),`);
+          } else {
+            php.push(`  "${_text}" => array(__("${_text}", '${opts.domain}')),`);
+          }
         }
-        
+
       });
 
       php.push(');');
 
-      if (!fs.existsSync(opts.path)){
-          fs.mkdirSync(opts.path);
+      if (!fs.existsSync(opts.path)) {
+        fs.mkdirSync(opts.path);
       }
 
-      fs.writeFile(opts.path + '/messages.php', php.join("\r\n"), (err) =>  {
-          if (err) throw err;
+      fs.writeFile(opts.path + '/messages.php', php.join("\r\n"), (err) => {
+        if (err) throw err;
       });
 
-      console.log( `✓ ${messages.length} string(s) extracted for WordPress.` );
+      console.log(`✓ ${messages.length} string(s) extracted for WordPress.`);
     }
   }
 }
